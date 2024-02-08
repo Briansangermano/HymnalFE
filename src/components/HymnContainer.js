e700de8923771b1569a1b38f624e93853826c422
briansangermano <briansangermano@gmail.com>
2024-02-08 15:42:09 -0300

    Implementacion de las alertas

--- src/components/HymnContainer.js
+++ src/components/HymnContainer.js
@@ -4,6 +4,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import HymnLetter from "./HymnLetter";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import {
   Typography,
   Box,
@@ -49,63 +51,86 @@ const HymnContainer = () => {
   const copiarAlPortapapeles = () => {
     const content = document.querySelector(".himno-content")?.innerText;
 
-    console.log("PASO", content);
-
     navigator.clipboard
       .writeText(content)
       .then(() => {
-        console.log("Contenido copiado al portapapeles:", content);
-        // Puedes mostrar un mensaje de éxito aquí si lo deseas
+        notifySuccess();
       })
-      .catch((error) => {
-        console.error("Error al copiar el contenido al portapapeles:", error);
-        // Puedes mostrar un mensaje de error aquí si lo deseas
+      .catch(() => {
+        notifyError();
       });
   };
+
+  const notifySuccess = () =>
+    toast.success("El himno fue copiado exitosamente", {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+
+  const notifyError = () =>
+    toast.error("Tuvimos un error al copiar le himno", {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
 
   return (
     <ThemeProvider theme={theme}>
       {himno ? (
-        <Box
-          sx={{
-            margin: "60px 13px 20px 13px",
-            padding: "15px 0 15px",
-            borderRadius: "10px",
-            background: "white",
-            boxShadow: "4px 6px 16px darkslategray",
-          }}
-        >
-          <Button
-            variant="outlined"
-            color="primary"
-            startIcon={<ArrowBackIosIcon />}
-            sx={stylesBackButton}
-            style={{
-              position: "absolute",
-              top: "15px",
-              left: "10px",
-              zIndex: "1000",
-              border: "0",
+        <>
+          <ToastContainer />
+          <Box
+            sx={{
+              margin: "60px 13px 20px 13px",
+              padding: "15px 0 15px",
+              borderRadius: "10px",
+              background: "white",
+              boxShadow: "4px 6px 16px darkslategray",
             }}
-            onClick={goBack}
           >
-            Back
-          </Button>
-          <Button
-            endIcon={<ContentCopyIcon />}
-            style={{
-              position: "absolute",
-              top: "15px",
-              right: "10px",
-              zIndex: "1000",
-              border: "0",
-            }}
-            onClick={copiarAlPortapapeles}
-          >
-            Copy
-          </Button>
-          <HymnLetter himno={himno} />
-        </Box>
+            <Button
+              variant="outlined"
+              color="primary"
+              startIcon={<ArrowBackIosIcon />}
+              sx={stylesBackButton}
+              style={{
+                position: "absolute",
+                top: "15px",
+                left: "10px",
+                zIndex: "1000",
+                border: "0",
+              }}
+              onClick={goBack}
+            >
+              Back
+            </Button>
+            <Button
+              endIcon={<ContentCopyIcon />}
+              style={{
+                position: "absolute",
+                top: "15px",
+                right: "10px",
+                zIndex: "1000",
+                border: "0",
+              }}
+              onClick={copiarAlPortapapeles}
+            >
+              Copy
+            </Button>
+            <HymnLetter himno={himno} />
+          </Box>
+        </>
       ) : (
         <Typography
           variant="h6"
